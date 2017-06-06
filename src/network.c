6c3e90f668cbd519e59940325c48ca974e6bcd3e
Patxi BERARD <patxi.berard@gmail.com>
2017-06-06 18:19:27 +0200

    HOTFIX WIN compilation errors about zEl : RENAMING by agentElements & change ipAddress initialization

--- src/network.c
+++ src/network.c
@@ -795,7 +795,7 @@ int mtic_startWithDevice(const char *networkDevice, int port){
             count = wcstombs(frindly_name, pCurrAddresses->FriendlyName, BUFSIZ );
             
             //If the friendly_name is the same of the networkDevice
-            if (strcmp(zEl->networkDevice, frindly_name) == 0)
+            if (strcmp(agentElements->networkDevice, frindly_name) == 0)
             {
                 pUnicast = pCurrAddresses->FirstUnicastAddress;
                 if (pUnicast != NULL)
@@ -807,9 +807,11 @@ int mtic_startWithDevice(const char *networkDevice, int port){
                             if (pUnicast->Address.lpSockaddr->sa_family == AF_INET)
                             {
                                 struct sockaddr_in *sa_in = (struct sockaddr_in *)pUnicast->Address.lpSockaddr;
-                                zEl->ipAddress = strdup(inet_ntop(AF_INET,&(sa_in->sin_addr),buff,bufflen));
+                                //TODO : check if OK
+//                                agentElements->ipAddress[0] = strdup(inet_ntop(AF_INET,&(sa_in->sin_addr),buff,bufflen));
+                                strncpy(agentElements->ipAddress, inet_ntoa(sa_in->sin_addr), IP_ADDRESS_LENGTH);
                                 free(frindly_name);
-                                mtic_debug("Connection on ip address %s on device %s\n", zEl->ipAddress, zEl->networkDevice);
+                                mtic_debug("Connection on ip address %s on device %s\n", agentElements->ipAddress, agentElements->networkDevice);
                             }
                         }
                         pUnicast = pUnicast->Next;
