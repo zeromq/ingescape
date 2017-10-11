853c632832021c9cc18de8256efe4b9ad201ed09
Patxi BERARD <patxi.berard@gmail.com>
2017-10-11 11:37:37 +0200

    Implement the agent mapping model

--- editor/apps/MASTIC-Editor/model/agentmappingm.cpp
+++ editor/apps/MASTIC-Editor/model/agentmappingm.cpp
@@ -16,14 +16,24 @@
 
 /**
  * @brief Constructor
+ * @param name
+ * @param version
+ * @param description
  * @param parent
  */
-AgentMappingM::AgentMappingM(QObject *parent) : QObject(parent)
+AgentMappingM::AgentMappingM(QString name,
+                             QString version,
+                             QString description,
+                             QObject *parent) : QObject(parent),
+    _name(name),
+    _version(version),
+    _description(description),
+    _md5Hash("")
 {
     // Force ownership of our object, it will prevent Qml from stealing it
     QQmlEngine::setObjectOwnership(this, QQmlEngine::CppOwnership);
 
-    qInfo() << "New Model of Agent Mapping...";
+    qInfo() << "New Model of Agent Mapping..."<< _name << "with version" << _version << "about" << _description;
 }
 
 
