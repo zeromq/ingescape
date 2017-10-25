2fbe7c15338aafa57c8db6129998542c9431bba6
Alexandre Lemort <lemort@ingenuity.io>
2017-10-25 10:07:37 +0200

    Setters must always be used to set a delete proof property

--- editor/apps/MASTIC-Editor/viewModel/mapbetweeniopvm.cpp
+++ editor/apps/MASTIC-Editor/viewModel/mapbetweeniopvm.cpp
@@ -18,25 +18,62 @@
 #include <QQmlEngine>
 #include <QDebug>
 
+
+/**
+ * @brief Constructor
+ * @param pointFrom
+ * @param pointTo
+ * @param parent
+ */
 MapBetweenIOPVM::MapBetweenIOPVM(PointMapVM *pointFrom, PointMapVM *pointTo, QObject *parent) : QObject(parent),
-    _pointFrom(pointFrom),
-    _pointTo(pointTo)
+    _pointFrom(NULL),
+    _pointTo(NULL)
 {
     // Force ownership of our object, it will prevent Qml from stealing it
     QQmlEngine::setObjectOwnership(this, QQmlEngine::CppOwnership);
 
-    qInfo() << "Map Between "<<_pointFrom->iopModel()->name()<<"->"<<_pointTo->iopModel()->name()<<"created";
+    setpointFrom(pointFrom);
+    setpointTo(pointTo);
+
+    if ((_pointFrom != NULL) && (_pointTo != NULL))
+    {
+        qInfo() << "Map Between "<<_pointFrom->iopModel()->name()<<"->"<<_pointTo->iopModel()->name()<<"created";
+    }
+    else
+    {
+        qWarning() << "Invalid map";
+    }
 }
 
+
+/**
+ * @brief Constructor
+ * @param pointFrom
+ * @param parent
+ */
 MapBetweenIOPVM::MapBetweenIOPVM(PointMapVM *pointFrom, QObject *parent) : QObject(parent),
-    _pointFrom(pointFrom)
+    _pointFrom(NULL)
 {
     // Force ownership of our object, it will prevent Qml from stealing it
     QQmlEngine::setObjectOwnership(this, QQmlEngine::CppOwnership);
 
-    qInfo() << "Map Between "<<_pointFrom->iopModel()->name()<<"created";
+    setpointFrom(pointFrom);
+
+    if (_pointFrom != NULL)
+    {
+        qInfo() << "Map Between "<<_pointFrom->iopModel()->name()<<"created";
+    }
+    else
+    {
+        qWarning() << "Invalid map";
+    }
 }
 
+
+
+/**
+ * @brief Destructor
+ */
 MapBetweenIOPVM::~MapBetweenIOPVM()
 {
     qInfo() << "Map destroyed";
