cc6c80404cd44e001931b509f02081cd75591d4b
Stéphane Valès <stephane@vales.fr>
2018-01-25 20:02:06 +0100

    add comments

--- src/include/mastic.h
+++ src/include/mastic.h
@@ -35,11 +35,11 @@ PUBLIC int mtic_stop(void);
 
 //agent name set and get
 PUBLIC int mtic_setAgentName(const char *name);
-PUBLIC char *mtic_getAgentName(void);
+PUBLIC char *mtic_getAgentName(void); //char* must be freed by caller
 
 //control agent state
 PUBLIC int mtic_setAgentState(const char *state);
-PUBLIC char *mtic_getAgentState(void);
+PUBLIC char *mtic_getAgentState(void); //char* must be freed by caller
 
 //mute the agent ouputs
 PUBLIC int mtic_mute(void);
@@ -62,8 +62,8 @@ PUBLIC void mtic_setCanBeFrozen(bool canBeFrozen);
 //instance and its hosting application:
 //1- using mtic_start* and mtic_stop from the hosting app
 //2- monitoring the status of mtic_Interrupted in the hosting app
-//3- using mtic_observeForcedStop below and providing a callback using parent thread
-//4- setting mtic_Interrupted from Mastic callbacks and arranging to call mtic_stop from main thread when mtic_Interrupted is set to true
+//3- using mtic_observeForcedStop below and providing a callback *using the parent thread*
+//4- setting mtic_Interrupted from Mastic callbacks and arranging to call mtic_stop from parent thread when mtic_Interrupted is set to true
 
 PUBLIC extern bool mtic_Interrupted;
 //register a callback when the agent is asked to stop on the network
@@ -159,7 +159,7 @@ PUBLIC bool mtic_checkInputExistence(const char *name);
 PUBLIC bool mtic_checkOutputExistence(const char *name);
 PUBLIC bool mtic_checkParameterExistence(const char *name);
 
-//observe IOP
+//observe writing to IOP
 typedef void (*mtic_observeCallback)(iop_t iopType, const char* name, iopType_t valueType, void* value, long valueSize, void* myData);
 PUBLIC int mtic_observeInput(const char *name, mtic_observeCallback cb, void *myData);
 PUBLIC int mtic_observeOutput(const char *name, mtic_observeCallback cb, void * myData);
@@ -177,18 +177,18 @@ PUBLIC bool mtic_isOutputMuted(const char *name);
 PUBLIC int mtic_loadDefinition (const char* json_str);
 PUBLIC int mtic_loadDefinitionFromPath (const char* file_path);
 PUBLIC int mtic_clearDefinition(void); //clears definition data for the agent
-PUBLIC char* mtic_getDefinition(void); //returns json string
-PUBLIC char *mtic_getDefinitionName(void);
-PUBLIC char *mtic_getDefinitionDescription(void);
-PUBLIC char *mtic_getDefinitionVersion(void);
-PUBLIC int mtic_setDefinitionName(char *name);
+PUBLIC char* mtic_getDefinition(void); //returns json string, must be freed by caller
+PUBLIC char *mtic_getDefinitionName(void); // must be freed by caller
+PUBLIC char *mtic_getDefinitionDescription(void); // must be freed by caller
+PUBLIC char *mtic_getDefinitionVersion(void); // must be freed by caller
+PUBLIC int mtic_setDefinitionName(char *name); 
 PUBLIC int mtic_setDefinitionDescription(char *description);
 PUBLIC int mtic_setDefinitionVersion(char *version);
 
 //edit the definition using the API
-PUBLIC int mtic_createInput(const char *name, iopType_t value_type, void *value, long size); //value must be copied in function
-PUBLIC int mtic_createOutput(const char *name, iopType_t type, void *value, long size); //value must be copied in function
-PUBLIC int mtic_createParameter(const char *name, iopType_t type, void *value, long size); //value must be copied in function
+PUBLIC int mtic_createInput(const char *name, iopType_t value_type, void *value, long size);
+PUBLIC int mtic_createOutput(const char *name, iopType_t type, void *value, long size);
+PUBLIC int mtic_createParameter(const char *name, iopType_t type, void *value, long size);
 
 PUBLIC int mtic_removeInput(const char *name);
 PUBLIC int mtic_removeOutput(const char *name);
@@ -201,10 +201,10 @@ PUBLIC int mtic_removeParameter(const char *name);
 PUBLIC int mtic_loadMapping (const char* json_str);
 PUBLIC int mtic_loadMappingFromPath (const char* file_path);
 PUBLIC int mtic_clearMapping(void); //clears mapping data for the agent
-PUBLIC char* mtic_getMapping(void); //returns json string
-PUBLIC char *mtic_getMappingName(void);
-PUBLIC char *mtic_getMappingDescription(void);
-PUBLIC char *mtic_getMappingVersion(void);
+PUBLIC char* mtic_getMapping(void); //returns json string, must be freed by caller
+PUBLIC char *mtic_getMappingName(void); // must be freed by caller
+PUBLIC char *mtic_getMappingDescription(void); // must be freed by caller
+PUBLIC char *mtic_getMappingVersion(void); // must be freed by caller
 
 //edit mapping using the API
 PUBLIC int mtic_setMappingName(char *name);
@@ -219,18 +219,22 @@ PUBLIC int mtic_removeMappingEntryWithName(const char *fromOurInput, const char 
 //////////////////////////////////////////////////
 //administration, configuration & utilities
 
+//Mastic library version
+//returns MAJOR*10000 + MINOR*100 + MICRO
+//displays MAJOR.MINOR.MICRO in console
 PUBLIC int mtic_version(void);
 
-//utility function to find network adapters with broadcast capabilities
+//Utility function to find network adapters with broadcast capabilities
 //to be used in mtic_startWithDevice
 PUBLIC void mtic_getNetdevicesList(char ***devices, int *nb);
 PUBLIC void mtic_freeNetdevicesList(char **devices, int nb);
 
-//Command line for the agent can be passed here for inclusion in the
-//agent's headers. If not set, header is initialized with exec path.
+//Agent command line can be passed here for inclusion in the
+//agent's headers. If not set, command line is initialized
+//with exec path without any parameter.
 PUBLIC void mtic_setCommandLine(const char *line);
 
-//By default, an agent notifies all the agent it maps. Each notification
+//By default, an agent notifies all the agents it maps. Each notification
 //makes the mapped agents publish their outputs (except for data & impulsions).
 //We allow to disable this notification to avoid side effects by agents frequently
 //changing their mapping.
@@ -240,8 +244,8 @@ PUBLIC void mtic_setNotifyMappedAgents(bool notify);
 PUBLIC void mtic_setVerbose(bool verbose); //log in console
 PUBLIC void mtic_setLogStream(bool stream); //log in socket
 PUBLIC void mtic_setLogInFile(bool useLogFile); //log in file
-PUBLIC void mtic_setUseColorVerbose (bool useColor); //use color in console
-void mtic_setLogPath(const char *path);
+PUBLIC void mtic_setUseColorVerbose (bool useColor); //use colors in console
+void mtic_setLogPath(const char *path); //for log file
 typedef enum {
     MTIC_LOG_TRACE = 0,
     MTIC_LOG_DEBUG,
@@ -250,7 +254,7 @@ typedef enum {
     MTIC_LOG_ERROR,
     MTIC_LOG_FATAL
 } mtic_logLevel_t;
-void mtic_setLogLevel (mtic_logLevel_t level);
+void mtic_setLogLevel (mtic_logLevel_t level); //default is MTIC_LOG_TRACE
 
 //void mtic_debug(const char*fmt, ...);
 void mtic_log(mtic_logLevel_t, const char*fmt, ...);
