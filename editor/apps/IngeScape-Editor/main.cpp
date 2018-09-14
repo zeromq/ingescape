a6ee877b848194597e1bd9e72701d891820bdec6
Stéphane Valès <stephane@vales.fr>
2018-09-14 15:03:48 +0200

    version update

--- editor/apps/IngeScape-Editor/main.cpp
+++ editor/apps/IngeScape-Editor/main.cpp
@@ -254,7 +254,7 @@ int main(int argc, char *argv[])
     app.setOrganizationName("Ingenuity i/o");
     app.setOrganizationDomain("ingenuity.io");
     app.setApplicationName("IngeScape-Editor");
-    app.setApplicationVersion("0.0.0");
+    app.setApplicationVersion("0.8.1");
 
     // - behavior when our last window is closed
     app.setQuitOnLastWindowClosed(true);
