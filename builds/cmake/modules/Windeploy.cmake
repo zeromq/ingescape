1839d0eb02ba30c6af855bc7a83261d93f91d1e5
Esteveny <esteveny@ingenuity.io>
2019-01-03 17:24:30 +0100

    Add shortcup for editor in windows, fix windows api folder and icon

--- builds/cmake/modules/Windeploy.cmake
+++ builds/cmake/modules/Windeploy.cmake
@@ -62,6 +62,7 @@ function(windeployqt target qml_src_dir)
     # windeployqt doesn't work correctly with the system runtime libraries,
     # so we fall back to one of CMake's own modules for copying them over
     set(CMAKE_INSTALL_UCRT_LIBRARIES TRUE)
+    set(CMAKE_INSTALL_SYSTEM_RUNTIME_DESTINATION .)
     include(InstallRequiredSystemLibraries)
     foreach(lib ${CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS})
         get_filename_component(filename "${lib}" NAME)
