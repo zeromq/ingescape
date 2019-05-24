40b5c4d1a654f9b86b7965131b4a313bb2c79f0f
ESTIA\p.berard <p.berard@estia.fr>
2019-05-24 11:33:02 +0200

    Rename the assembly sample name.

--- bindings/c#/sample/CSharpSampleAgent/CSharpSampleAgent/Properties/AssemblyInfo.cs
+++ bindings/c#/sample/CSharpSampleAgent/CSharpSampleAgent/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("SendCSharpAgent")]
+[assembly: AssemblyTitle("CSharpSampleAgent")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SendCSharpAgent")]
+[assembly: AssemblyProduct("CSharpSampleAgent")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
