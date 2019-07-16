2877741e3d7917e9da506c3cec4c9cb702e50bad
Mathieu Soum <soum@ingenuity.io>
2019-07-16 15:18:34 +0200

    Deleting a task from DB also deletes its related independent variables

--- editor/apps/IngeScape-Assessments/controller/task/taskscontroller.cpp
+++ editor/apps/IngeScape-Assessments/controller/task/taskscontroller.cpp
@@ -160,10 +160,10 @@ void TasksController::deleteTask(TaskM* task)
             setselectedTask(nullptr);
         }
 
-        //FIXME Delete variables too
+        //TODO Remove dependent variables from DB
 
-        // Remove task from DB
-        const char* query = "DELETE FROM ingescape.task WHERE id_experimentation = ? AND id = ?;";
+        // Remove independent_var from DB
+        const char* query = "DELETE FROM ingescape.independent_var WHERE id_experimentation = ? AND id_task = ?;";
         CassStatement* cassStatement = cass_statement_new(query, 2);
         cass_statement_bind_uuid(cassStatement, 0, task->getExperimentationCassUuid());
         cass_statement_bind_uuid(cassStatement, 1, task->getCassUuid());
@@ -171,6 +171,27 @@ void TasksController::deleteTask(TaskM* task)
         // Execute the query or bound statement
         CassFuture* cassFuture = cass_session_execute(_modelManager->getCassSession(), cassStatement);
         CassError cassError = cass_future_error_code(cassFuture);
+        if (cassError == CASS_OK)
+        {
+            qInfo() << "Independent variables for task" << task->name() << "has been successfuly deleted from the DB";
+        }
+        else {
+            qCritical() << "Could not delete the task" << task->name() << "from the DB:" << cass_error_desc(cassError);
+        }
+
+        // Clean-up cassandra objects
+        cass_future_free(cassFuture);
+        cass_statement_free(cassStatement);
+
+        // Remove task from DB
+        query = "DELETE FROM ingescape.task WHERE id_experimentation = ? AND id = ?;";
+        cassStatement = cass_statement_new(query, 2);
+        cass_statement_bind_uuid(cassStatement, 0, task->getExperimentationCassUuid());
+        cass_statement_bind_uuid(cassStatement, 1, task->getCassUuid());
+
+        // Execute the query or bound statement
+        cassFuture = cass_session_execute(_modelManager->getCassSession(), cassStatement);
+        cassError = cass_future_error_code(cassFuture);
         if (cassError == CASS_OK)
         {
             qInfo() << "Task" << task->name() << "has been successfuly delete from the DB";
