7aeb3fd01b3ffc3475cf3ad8f91378455cb743be
Esteveny <esteveny@ingenuity.io>
2019-09-06 11:49:05 +0200

    Update gitlab ci from vs 2015 to vs 2017

--- win32_cmake_build.bat
+++ win32_cmake_build.bat
@@ -3,7 +3,7 @@ rem Batch to compile ingescape on windows
 rem To compile ingescape with windows juste edit next 3 PATHS
 rem IGS_*_DIRECTORY is the path to czmq, sodium, zmq, yajl and zyre includes and libraries
 rem CMAKE_EXE is the path to cmake exe
-rem Project is configured for 64bits system. Remove Win64 in -G"Visual Studio 14 2015 Win64" for 32bits
+rem Project is configured for 64bits system. Remove Win64 in -G"Visual Studio 15 2017 Win64" for 32bits
 
 rem Define configurables path
 set IGS_INCLUDE_DIRECTORY=C:\Program Files (x86)\Ingescape\include
@@ -27,7 +27,7 @@ rem Move in build folder
 cd "%batchpath%\build"
 
 rem Launch cmake command
-%CMAKE_EXE% -G"Visual Studio 14 2015" ..  -DZYRE_LIBRARIES="%IGS_LIBRARY_DIRECTORY%zyre.lib" -DZYRE_INCLUDE_DIRS="%IGS_INCLUDE_DIRECTORY%" -DCZMQ_LIBRARIES="%IGS_LIBRARY_DIRECTORY%czmq.lib" -DCZMQ_INCLUDE_DIRS="%IGS_INCLUDE_DIRECTORY%" -DLIBSODIUM_LIBRARIES="%IGS_LIBRARY_DIRECTORY%libsodium.lib" -DLIBSODIUM_INCLUDE_DIRS="%IGS_INCLUDE_DIRECTORY%" -DLIBZMQ_LIBRARIES="%IGS_LIBRARY_DIRECTORY%libzmq-v140-mt-4_3_1.lib" -DLIBZMQ_INCLUDE_DIRS="%IGS_INCLUDE_DIRECTORY%" -DYAJL_LIBRARIES="%IGS_LIBRARY_DIRECTORY%yajl.lib" -DYAJL_INCLUDE_DIRS="%IGS_INCLUDE_DIRECTORY%"
+%CMAKE_EXE% -G"Visual Studio 15 2017" ..  -DZYRE_LIBRARIES="%IGS_LIBRARY_DIRECTORY%zyre.lib" -DZYRE_INCLUDE_DIRS="%IGS_INCLUDE_DIRECTORY%" -DCZMQ_LIBRARIES="%IGS_LIBRARY_DIRECTORY%czmq.lib" -DCZMQ_INCLUDE_DIRS="%IGS_INCLUDE_DIRECTORY%" -DLIBSODIUM_LIBRARIES="%IGS_LIBRARY_DIRECTORY%libsodium.lib" -DLIBSODIUM_INCLUDE_DIRS="%IGS_INCLUDE_DIRECTORY%" -DLIBZMQ_LIBRARIES="%IGS_LIBRARY_DIRECTORY%libzmq-v141-mt-4_3_1.lib" -DLIBZMQ_INCLUDE_DIRS="%IGS_INCLUDE_DIRECTORY%" -DYAJL_LIBRARIES="%IGS_LIBRARY_DIRECTORY%yajl.lib" -DYAJL_INCLUDE_DIRS="%IGS_INCLUDE_DIRECTORY%"
 rem Compile ingescape
 %CMAKE_EXE% --build . --target ALL_BUILD --config Release
 
