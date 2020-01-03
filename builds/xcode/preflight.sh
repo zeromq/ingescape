bac80ffae4a751482074e46ac822f0709f592ad8
Stéphane Valès <stephane@vales.fr>
2020-01-03 15:37:01 +0100

    update packaging with pre/post flight scripts (currently empty) and welcome text

---
+++ builds/xcode/preflight.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#Due to privileges limitations in Homebrew, we cannot
+#execute automatic removal of brew-managed zyre and dependencies
+#automatically.
+#FILE=/usr/local/bin/brew
+#if [[ -f "$FILE" ]]; then
+#    brew remove -f zyre czmq zeromq libsodium
+#fi
