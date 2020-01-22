b1855563fd53acb6e3c07e5c129ae2fe4645822f
Stéphane Valès <stephane@vales.fr>
2020-01-22 14:15:59 +0100

    correct variable for windows

--- src/network.c
+++ src/network.c
@@ -105,7 +105,7 @@ void network_Lock(void)   {
     }
 #elif (defined WIN32 || defined _WIN32)
     if (bus_zyreMutex == NULL){
-        if (pthread_mutex_init(&bus_zyreMutex) != 0){
+        if (pthread_mutex_init(&network_Mutex) != 0){
             igs_fatal("mutex init failed");
             assert(false);
             return;
