a2ce7b69ff0cdefb45e9b9a70acc2353ce64b3c8
Stéphane Valès <stephane@vales.fr>
2020-02-11 16:11:36 +0000

    add projects for ZeroMQ suite for in-depth debugging capabilities

--- dependencies/macos/installDependencies.sh
+++ dependencies/macos/installDependencies.sh
@@ -9,18 +9,24 @@ mkdir build
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -DWITH_LIBSODIUM=ON -DENABLE_DRAFTS=ON
 sudo make -C build install
 sudo rm -Rf build
+mkdir -p builds/xcode
+cmake -S . -B builds/xcode -DCMAKE_BUILD_TYPE=Debug -DWITH_LIBSODIUM=ON -DENABLE_DRAFTS=ON -G "Xcode"
 
 cd ../czmq
 mkdir build
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -DENABLE_DRAFTS=ON
 sudo make -C build install
 sudo rm -Rf build
+mkdir -p builds/xcode
+cmake -S . -B builds/xcode -DCMAKE_BUILD_TYPE=Debug -DENABLE_DRAFTS=ON -G "Xcode"
 
 cd ../zyre
 mkdir build
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -DENABLE_DRAFTS=ON
 sudo make -C build install
 sudo rm -Rf build
+mkdir -p builds/xcode
+cmake -S . -B builds/xcode -DCMAKE_BUILD_TYPE=Debug -DENABLE_DRAFTS=ON -G "Xcode"
 
 cd ../ingescape/dependencies/macos
 
