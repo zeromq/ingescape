2ea2928163ebfc8fc76dac8c92a0a0bfe6f06cb0
Mathieu Soum <soum@ingenuity.io>
2020-08-13 09:47:08 +0200

    Extract bindings into a new repo: ssh://git@gitlab.ingescape.com:22222/I2/bindings.git

--- bindings/python/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#python 3 has to be installed on your system before continuing.
-#ingeScape has to be installed on your system before continuing so do libsodium zyre and czmq
-
-cd ./ingescapeWrapper
-python3 setup.py install
