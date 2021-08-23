6ada6d559227640be356a505c0ab11dfd2cc113a
abroutin <broutin@ingenuity.io>
2021-08-23 16:56:49 +0200

    Add sodium and zmq solutions
    Add sodium and zmq visual studio solutions to the dependencies folder

---
+++ builds/visual-studio/dependencies/libzmq/CMakeFiles/Export/CMake/ZeroMQTargets-debug.cmake
@@ -0,0 +1,29 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Debug".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "libzmq" for configuration "Debug"
+set_property(TARGET libzmq APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(libzmq PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/libzmq-v141-mt-gd-4_3_5.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/libzmq-v141-mt-gd-4_3_5.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS libzmq )
+list(APPEND _IMPORT_CHECK_FILES_FOR_libzmq "${_IMPORT_PREFIX}/lib/libzmq-v141-mt-gd-4_3_5.lib" "${_IMPORT_PREFIX}/bin/libzmq-v141-mt-gd-4_3_5.dll" )
+
+# Import target "libzmq-static" for configuration "Debug"
+set_property(TARGET libzmq-static APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(libzmq-static PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C;CXX;RC"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libzmq-v141-mt-sgd-4_3_5.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS libzmq-static )
+list(APPEND _IMPORT_CHECK_FILES_FOR_libzmq-static "${_IMPORT_PREFIX}/lib/libzmq-v141-mt-sgd-4_3_5.lib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
