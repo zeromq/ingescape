9bcffe1e0a5a5f2f0736b0cf5e340fe848cc951e
Stéphane Valès <7755128+stvales@users.noreply.github.com>
2022-04-29 10:24:51 +0200

    version upgrade after updating dependencies and solving bug with mutexes

--- include/ingescape.h
+++ include/ingescape.h
@@ -32,7 +32,7 @@
 //  INGESCAPE version macros for compile-time API detection
 #define INGESCAPE_VERSION_MAJOR 3
 #define INGESCAPE_VERSION_MINOR 0
-#define INGESCAPE_VERSION_PATCH 2
+#define INGESCAPE_VERSION_PATCH 3
 
 #define INGESCAPE_MAKE_VERSION(major, minor, patch) \
 ((major) * 10000 + (minor) * 100 + (patch))
