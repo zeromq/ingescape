38b466b7b2d62be686732bfa1adedb148df63658
Stéphane Valès <7755128+stvales@users.noreply.github.com>
2023-04-21 09:24:17 +0200

    make bootstrap scripts more portable

--- install_on_system.sh
+++ install_on_system.sh
@@ -1,4 +1,4 @@
-echo "Deploying ingescape library and its dependencies in $CODEROOT/sysroot"
+echo "Deploying ingescape library and its dependencies on the system"
 mkdir build
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DOSX_UNIVERSAL=ON -DWITH_DEPS=ON
 sudo make -j8 -C build install
