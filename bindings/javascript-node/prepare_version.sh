5943ebcdbea573bfab63493051e8ccddbeb60df0
Mathieu SOUM <mathieu.soum@gmail.com>
2023-09-14 13:52:33 +0200

    Problem: NodeJS binding version does not follow the ingescape.h version (#75)
    * Make NodeJS binding use the version in ingescape.h
    
    * Add prepare version script to CI jobs
    
    ---------
    
    Co-authored-by: Mathieu SOUM <soum@ingenuity.io>

---
+++ bindings/javascript-node/prepare_version.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR=$(cd `dirname $0`; pwd)
+SCRIPT_NAME=$(basename $0)
+
+function print_usage {
+    echo "Usage: $SCRIPT_NAME [-h]"
+    echo "  -h               : Show this help message and exit."
+}
+
+while getopts ":h" opt
+do
+    case "${opt}" in
+        h) # Help option
+            print_usage
+            exit 0
+            ;;
+        :) # Option with missing argument
+            echo "ERROR: -${OPTARG} requires an argument"
+            print_usage
+            exit 100
+            ;;
+        *) # Unknown option
+            echo "ERROR: unknown parameter -${OPTARG}"
+            print_usage
+            exit 110
+            ;;
+    esac
+done
+
+(
+  cd $SCRIPT_DIR
+
+  IGS_MAJOR=$(grep "#define INGESCAPE_VERSION_MAJOR" ../../include/ingescape.h | cut -d' ' -f3)
+  IGS_MINOR=$(grep "#define INGESCAPE_VERSION_MINOR" ../../include/ingescape.h | cut -d' ' -f3)
+  IGS_PATCH=$(grep "#define INGESCAPE_VERSION_PATCH" ../../include/ingescape.h | cut -d' ' -f3)
+
+  sed -i .orig "s/\"version\": \".*\",/\"version\": \"$IGS_MAJOR.$IGS_MINOR.$IGS_PATCH\",/" package.json
+  cat package.json
+)
+
+exit 0
+
