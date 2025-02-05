0672f31ed8d225b5482c1a614f174107591cb961
Aurélien Broutin <67375035+abroutin@users.noreply.github.com>
2025-02-05 17:26:36 +0100

    Change default rights to 0777 for IPC folder on unix systems

--- src/igs_network.c
+++ src/igs_network.c
@@ -3240,6 +3240,11 @@ void s_init_loop (igs_core_context_t *context)
             return;
         }
     }
+    
+    int result = chmod(context->network_ipc_folder_path, 0777);
+    if (result != EXIT_SUCCESS)
+        igs_error("failed chmod 0777 for IPC folder at '%s'", context->network_ipc_folder_path);
+
     s_lock_zyre_peer (__FUNCTION__, __LINE__);
     context->network_ipc_full_path = (char *) zmalloc (strlen (context->network_ipc_folder_path) + strlen (zyre_uuid (context->node)) + 2);
     sprintf (context->network_ipc_full_path, "%s/%s",
@@ -4719,6 +4724,11 @@ void igs_set_ipc_dir (const char *path)
     core_init_agent ();
     assert (path);
     model_read_write_lock(__FUNCTION__, __LINE__);
+    if (core_context->network_actor) {
+        igs_error ("IPC folder path cannot be changed while the agent is running");
+        model_read_write_unlock(__FUNCTION__, __LINE__);
+        return;
+    }
     if (core_context->network_ipc_folder_path == NULL
         || !streq (path, core_context->network_ipc_folder_path)) {
         if (*path == '/') {
@@ -4736,8 +4746,11 @@ void igs_set_ipc_dir (const char *path)
                     free (core_context->network_ipc_folder_path);
                 core_context->network_ipc_folder_path = strdup (path);
             } else if (core_context->network_ipc_folder_path)
-                igs_error ("IPC dir remains set to %s", core_context->network_ipc_folder_path);
-
+                igs_error ("IPC folder remains set to %s", core_context->network_ipc_folder_path);
+            
+            int result = chmod(core_context->network_ipc_folder_path, 0777);
+            if (result != EXIT_SUCCESS)
+                igs_error("failed chmod 0777 for IPC folder at '%s'", core_context->network_ipc_folder_path);
         }else
             igs_error ("IPC folder path must be absolute (invalid path: %s)", path);
     }
